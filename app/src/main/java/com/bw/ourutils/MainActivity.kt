33a3246cd37e80f8a39fcc71efaa7bbdbdc33ba8
q1745 <1745984453@qq.com>
2021-08-11 16:19:40 +0800

    1

--- app/src/main/java/com/bw/ourutils/MainActivity.kt
+++ app/src/main/java/com/bw/ourutils/MainActivity.kt
@@ -2,10 +2,14 @@ package com.bw.ourutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        log()
     }
 }
